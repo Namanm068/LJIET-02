6d2a1e4b04f1d82f6d45c3ccb42851d012ca2801
Namanm068 <133661337+Namanm068@users.noreply.github.com>
2023-05-31 14:18:03 +0530

    Create SwitchCase.java

---
+++ SwitchCase.java
@@ -0,0 +1,35 @@
+public class swit {
+
+    public static void main(String[] args) {
+
+        int days;
+
+        switch(days){
+
+            case 31:
+
+                System.out.println("January,March,May,July,August,October,December");
+
+                break;
+
+            case 30:
+
+                System.out.println("April,June,September,Novemnber");
+
+                break;
+
+            case 29:
+
+                System.out.println("February");
+
+                break;
+
+            default:
+
+                System.out.println("Enter valid days");
+
+        }
+
+    }
+
+}
